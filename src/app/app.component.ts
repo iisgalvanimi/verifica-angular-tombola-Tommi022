300f48eacb4d3c890dbd93f4d6b37db148f24c3c
Tommi022 <falcione.tommaso@iisgalvanimi.edu.it>
2022-03-10 09:05:47 +0000

    finito

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-Empty-Project';
+  list: string[] = ['']
+  testo: string=""
+  last : string= ""
+  constructor(){}
+
+  onClick(){
+    this.list.push(this.testo)
+    this.last = this.testo
+  }
 }
